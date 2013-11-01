8e6ab245b9c7f5b393b4b241437dfea041db0ab3
Jonathan Morley <morley.jonathan@gmail.com>
2013-11-01 17:30:41 +0000

    brute force problem 2

--- problem2/problem2.py
+++ problem2/problem2.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
 import sys, getopt,itertools
-from itertools import permutations,chain,repeat,islice
+from operator import itemgetter
+from itertools import combinations
 
 def main(argv):
     inputfile = ''
@@ -19,8 +20,17 @@ def main(argv):
             inputfile = arg
     
     with open(inputfile) as f:
-        generation, overhead, budget = list(f)
-        print(generation)
+        generation, overhead, budget = [x.rstrip() for x in list(f)]
+        generation = [int(x) for x in generation[1:-1].split(',')]
+        overhead = [int(x) for x in overhead[1:-1].split(',')]
+        cards = [x for x in zip(generation, overhead)]
+        print(time(cards))
+        combos = combinations(cards, 9)
+        print(min([time(combo) for combo in combos]))
+def time(cards):
+    gentime = sum([x for x,_ in cards])
+    overheadtime = sum([x for _,x in cards]) * (len(cards) - 1)
+    return gentime + overheadtime
 
 if __name__ == "__main__":
     main(sys.argv[1:])
